37e980f21ff85902178aeb1abf7f5301874b2420
Samuel Bueno <samuel@MacBook-Pro-de-Samuel.local>
2022-11-08 20:04:14 -0300

    dgimplantes

--- pages/index.js
+++ pages/index.js
@@ -157,8 +157,11 @@ export default function Home() {
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
       </main>
+<Footer>
+<title>Atualizado em 08/11/2022</title>
 
-      <title>Atualizado em 08/11/2022</title>
+</Footer>
+     
     </div>
     
   )
